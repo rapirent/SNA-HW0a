bc37da373b656824f44df417ebb658472be46a93
rapirent <rapirent@gmail.com>
2018-03-18 21:45:41 +0800

    handle with the `nan` problem

--- 丁國騰_hw0a.py
+++ 丁國騰_hw0a.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 
+import math
+
 def csv_manipulator(csv_file):
     """Read a csv file object and create a score summary dictionary
 
@@ -16,19 +18,16 @@ def csv_manipulator(csv_file):
     seasons_count = {}
     score = {}
     year_table = {}
-    # found the year list
+    # find years having a play
     for year in reader['year']:
         if year not in year_table:
             year_table[year] = {}
     for row in reader.index.tolist():
-        for col in reader.columns:
-            # clean the data
-            if reader[col][row] == 'NULL':
-                reader[col][row] = 0
         player_name = reader['firstname'][row] + ' ' + reader['lastname'][row]
-        # initilize the collectors
-        if player_name not in seasons_count:
-            seasons_count[player_name] = score_summary[player_name] = score[player_name] = 0
+        # seasons_count to count the players' seasons number
+        if player_name not in seasons_count.keys():
+            seasons_count[player_name] = 0
+            score[player_name] = 0
         if player_name not in year_table[reader['year'][row]]:
             year_table[reader['year'][row]][player_name] = {'pts': 0,
                                                             'reb': 0,
@@ -43,20 +42,26 @@ def csv_manipulator(csv_file):
                                                             'gp': 0}
         for key in year_table[reader['year'][row]][player_name].keys():
             # add the value in same season
-            year_table[reader['year'][row]][player_name][key] += reader[key][row]
-
+            if reader[key][row] == 'NULL':
+                year_table[reader['year'][row]][player_name][key] = 0
+            else:
+                year_table[reader['year'][row]][player_name][key] += reader[key][row]
     for year in year_table.keys():
         for player_name in year_table[year].keys():
             seasons_count[player_name] += 1
-            score[player_name] += ((year_table[year][player_name]['pts'] + year_table[year][player_name]['reb'] +
-                                    year_table[year][player_name]['asts'] + year_table[year][player_name]['stl'] +
-                                    year_table[year][player_name]['blk'] - (year_table[year][player_name]['fga'] -
-                                    year_table[year][player_name]['fgm'] + year_table[year][player_name]['fta'] -
-                                    year_table[year][player_name]['ftm'] + year_table[year][player_name]['turnover'])) /
-                                    year_table[year][player_name]['gp'])
-    for name in seasons_count:
+            value = ((year_table[year][player_name]['pts'] + year_table[year][player_name]['reb'] +
+                      year_table[year][player_name]['asts'] + year_table[year][player_name]['stl'] +
+                      year_table[year][player_name]['blk'] - (year_table[year][player_name]['fga'] -
+                      year_table[year][player_name]['fgm'] + year_table[year][player_name]['fta'] -
+                      year_table[year][player_name]['ftm'] + year_table[year][player_name]['turnover'])) /
+                      year_table[year][player_name]['gp'])
+            if math.isnan(value):
+                score[player_name] += 0
+            else:
+                score[player_name] += value
+    for name in seasons_count.keys():
         score_summary[name] = score[name] / seasons_count[name]
-    return {k: v for (v, k) in score_summary.items()}
+    return {score: name for (name, score) in score_summary.items()}
 
 def score_sort(score_dict):
     """use the python sorted function to sort the score dictionary
@@ -74,7 +79,6 @@ def score_sort(score_dict):
 if __name__ == '__main__':
     s = csv_manipulator('nba_data.csv')
     output = open('丁國騰_hw0a.txt', 'w')
-    # print(list(enumerate(score_sort(s))))
     for i, (name, score) in enumerate(score_sort(s)):
         if i < 20:
             output.write('RANK {0:2d}\t{1:>19s}\t{2:.2f}\n'.format(i+1, name, score))
